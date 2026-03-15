120d9b40ab36e764f41f739fece06610b7584b79
srithikakathiresan <srithikakathiresan@gmail.com>
2026-03-15 17:13:55 +0530

    Add files via upload

---
+++ E-9 Search an element in an array binary search.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, i, element;
+    int low = 0, high, mid, found = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter elements in sorted order:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &element);
+
+    high = n - 1;
+
+    while(low <= high) {
+        mid = (low + high) / 2;
+
+        if(arr[mid] == element) {
+            printf("Element found at position %d", mid + 1);
+            found = 1;
+            break;
+        }
+        else if(arr[mid] < element) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if(found == 0) {
+        printf("Element not found");
+    }
+
+    return 0;
+}
